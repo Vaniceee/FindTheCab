3d6b5c1d34cf6652d53b281308a5ce050c3caf58
Vaniceee <vanarebrik2@gmail.com>
2026-06-23 15:42:28 +0300

    Зробив текстурку для НПС(Сподіваюсь працює)

--- NPC.cpp
+++ NPC.cpp
@@ -5,12 +5,37 @@ NPC::NPC(Vector2 pos, const std::string& text)
 {
     position = pos;
     dialogue = text;
-    color = RED;
+    color = WHITE; 
+
+    
+    texture = LoadTexture("assets/16x16 Idle.png");
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+}
+
+NPC::~NPC()
+{
+   
+    UnloadTexture(texture);
 }
 
 void NPC::Draw()
 {
-    DrawCircleV(position, 10, color);
+
+    float frameSize = (float)texture.height;
+
+   
+    Rectangle sourceRec = { 0.0f, 0.0f, frameSize, frameSize };
+
+
+    Rectangle destRec = {
+        position.x,
+        position.y,
+        frameSize * scale,
+        frameSize * scale
+    };
+
+   
+    DrawTexturePro(texture, sourceRec, destRec, { 0.0f, 0.0f }, 0.0f, WHITE);
 }
 
 bool NPC::IsPlayerNear(Vector2 playerPos, float range)
--- NPC.h
+++ NPC.h
@@ -10,7 +10,12 @@ public:
     Color color;
 
     NPC(Vector2 pos, const std::string& text);
+    ~NPC(); 
 
     void Draw();
-    bool IsPlayerNear(Vector2 playerPos, float range = 50.0f);
+    bool IsPlayerNear(Vector2 playerPos, float range = 60.0f); 
+
+private:
+    Texture2D texture;      
+    const float scale = 3.0f; 
 };
